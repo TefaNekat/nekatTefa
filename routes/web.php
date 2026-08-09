e7a9c6a1265e93709bf0dd47cbf1d178f82b2466
Nabil Yusra <nabilazura3@gmail.com>
2026-08-09 21:47:42 +0700

    Add routes for Home, About, Product, Contact, and Lead storage

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\LeadController;
+use App\Http\Controllers\Public\AboutController;
+use App\Http\Controllers\Public\ContactController;
+use App\Http\Controllers\Public\HomeController;
+use App\Http\Controllers\Public\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/{slug}', [ProductController::class, 'show'])->name('product.show');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+Route::post('/hubungi-admin/{produk}', [LeadController::class, 'store'])
+    ->middleware(['auth:web', \App\Http\Middleware\EnsureCustomerAuthenticated::class])
+    ->name('lead.store');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
