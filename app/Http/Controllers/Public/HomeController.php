bc85424840bcd3d96fc1a8fdd0cdd98ffa61525a
Nabil Yusra <nabilazura3@gmail.com>
2026-08-22 22:50:53 +0700

    Add Page model retrieval in HomeController index method for home page

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
 use App\Models\Jurusan;
+use App\Models\Page;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -11,7 +12,8 @@ class HomeController extends Controller
     public function index(): View
     {
         $jurusans = Jurusan::all();
+        $page = Page::where('slug', 'home')->first();
 
-        return view('public.home', compact('jurusans'));
+        return view('public.home', compact('jurusans', 'page'));
     }
 }
