f203d9072d60228e34f57a934a6c2753470114ca
Nabil Yusra <nabilazura3@gmail.com>
2026-08-22 22:49:51 +0700

    Retrieve contact page data in ContactController index method

--- app/Http/Controllers/Public/ContactController.php
+++ app/Http/Controllers/Public/ContactController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
 use Illuminate\View\View;
 
 class ContactController extends Controller
 {
     public function index(): View
     {
-        return view('public.contact');
+        $page = Page::where('slug', 'contact')->first();
+
+        return view('public.contact', compact('page'));
     }
 }
